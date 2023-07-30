0e1c546760ab3aa1b5833ce9562686abe0256e1f
sureshpython07 <sureshpython07@gmail.com>
2023-07-30 17:47:48 +0530

    file for jen exaple

---
+++ test.py
@@ -0,0 +1,12 @@
+origianl_data = {           
+'eno':1006,
+'ename':'Palli',
+'esal':3000.0,
+'eadd':'Hyderabad'
+        }
+pro={
+    'esal':70000,
+    'eadd':'Kolkatha'
+    }
+origianl_data.update(pro)
+origianl_data
